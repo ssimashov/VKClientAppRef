68780fa3056876a4f952970cb834020bbc1106e9
Sergey Simashov <simashov@gmail.com>
2023-02-19 21:36:35 +0300

    Added News VC

---
+++ VKClientApp/VC/News/NewsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewsViewController.swift
+//  VKClientAppCS
+//
+//  Created by Sergey Simashov on 19.02.2023.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet var TableView: UIView!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
